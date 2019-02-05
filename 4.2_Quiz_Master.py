2f43b307059c026cb7975206902e44c89024a7dc
aceirino <aidankugley@gmail.com>
2019-02-05 12:19:16 -0600

    reeeeeeeeeeeeeeeee2

--- 4.2_Quiz_Master.py
+++ 4.2_Quiz_Master.py
@@ -4,3 +4,5 @@ QUIZ MASTER PROJECT
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+question1=float(input("what does 100-50 equal? "))
+question2=
--- Grading Program.py
+++ Grading Program.py
@@ -1,4 +1,22 @@
-semgrade=int(input("semgrade:"))
-finalexam=int(input("finalexam:"))
-examworth=int(input("examworth:"))
-print(((100-examworth)*semgrade+(examworth*finalexam))/100,"%")
+grade=float(input("what is your grade?"))
+test=float(input("what will you get on the test?"))
+worth=float(input("what is the test worth?"))
+
+worth=worth/100
+overall=test*worth+grade*(1-worth)
+letter=""
+
+if overall>=100 and worth==0:
+    letter="A+ you are a lucky guesser"
+elif overall>=90:
+    letter="A"
+elif overall>=80:
+    letter="B"
+elif overall>=70:
+    letter="C"
+elif overall>=60:
+    letter="D"
+else:
+    letter="F please transfer to johnston!"
+
+print("your overall grade is...",overall,"%", letter)
