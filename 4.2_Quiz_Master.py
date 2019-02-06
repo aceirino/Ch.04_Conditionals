0bcba4361fed0a4c0d6c2ffe23217ec29a0c9211
aceirino <aidankugley@gmail.com>
2019-02-06 14:46:17 -0600

    reeeeeeeeeeeeeeeee3

--- 4.2_Quiz_Master.py
+++ 4.2_Quiz_Master.py
@@ -5,4 +5,6 @@ The criteria for the project are on the website. Make sure you test this quiz wi
 two of your student colleagues before you run it by your instructor.
 '''
 question1=float(input("what does 100-50 equal? "))
-question2=
+question2=input("what is the last name of our teacher?")
+question3=input("what is the largest number? A. 567 B. 678 C. 543 D. 679")
+question4=float(input("how old is Aidan?"))
